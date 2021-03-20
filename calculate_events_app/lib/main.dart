183267598fd774b6a5a073efbad869eb069d1ef7
dandy <danielsbarbosa27@gmail.com>
2021-03-20 13:41:55 -0300

    DevicePreview/Firebase Config ok

---
+++ calculate_events_app/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(DevicePreview(
+    builder: (_) => MyApp()
+  ));
+
+  Firestore.instance.collection('teste').add({'teste': 'teste'});
+
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculate Events',
+      theme: ThemeData(
+        primaryColor: Color(0xFF0561F9),
+        accentColor: Color(0xFFD90D69)
+      ),
+      builder: DevicePreview.appBuilder,
+      locale: DevicePreview.locale(context),
+      home: Container()
+    );
+  }
+}
