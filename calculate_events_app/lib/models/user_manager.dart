ab011494624a15acb261cda864b4447962c1054d
dandy <danielsbarbosa27@gmail.com>
2021-03-22 21:41:13 -0300

    Recuperando os dados do login

--- calculate_events_app/lib/models/user_manager.dart
+++ calculate_events_app/lib/models/user_manager.dart
@@ -1,30 +1,32 @@
 import 'package:calculate_events_app/helpers/firebase_errors.dart';
 import 'package:calculate_events_app/models/user.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 
 class UserManager extends ChangeNotifier {
-
-  UserManager(){
+  UserManager() {
     _loadCurrentUser();
   }
 
-  FirebaseUser user;
-
   final FirebaseAuth auth = FirebaseAuth.instance;
+  final Firestore firestore = Firestore.instance;
+
+  User user;
 
   bool _loading = false;
+
   bool get loading => _loading;
 
   Future<void> signIn({User user, Function onFail, Function onSuccess}) async {
-    loading =  true;
+    loading = true;
 
     try {
       final AuthResult result = await auth.signInWithEmailAndPassword(
           email: user.email, password: user.password);
 
-      this.user = result.user;
+      await _loadCurrentUser(firebaseUser : result.user);
 
       onSuccess();
     } on PlatformException catch (e) {
@@ -34,39 +36,41 @@ class UserManager extends ChangeNotifier {
     loading = false;
   }
 
-  Future<void> signUp({ User user, Function onFail, Function onSuccess}) async {
-    loading =true;
-  try {
-    final AuthResult result = await auth.createUserWithEmailAndPassword(
-        email: user.email, password: user.password);
-
-   user.id = result.user.uid;
-
-   await user.saveData();
+  Future<void> signUp({User user, Function onFail, Function onSuccess}) async {
+    loading = true;
+    try {
+      final AuthResult result = await auth.createUserWithEmailAndPassword(
+          email: user.email, password: user.password);
 
 
+      user.id = result.user.uid;
+      this.user = user;
 
-  onSuccess();
-  } on PlatformException catch (e) {
-    onFail(getErrorString(e.code));
+      await user.saveData();
+
+      onSuccess();
+    } on PlatformException catch (e) {
+      onFail(getErrorString(e.code));
+    }
+    loading = false;
   }
-  loading = false;
-  }
 
-
-  set loading (bool value){
+  set loading(bool value) {
     _loading = value;
     notifyListeners();
   }
 
-  Future<void> _loadCurrentUser() async{
-    final FirebaseUser currentUser = await auth.currentUser();
-    if(currentUser != null){
-      user = currentUser;
-      print(user.uid);
+  Future<void> _loadCurrentUser({FirebaseUser firebaseUser}) async {
+    final FirebaseUser currentUser = firebaseUser ?? await auth.currentUser();
+    if (currentUser != null) {
+      final DocumentSnapshot docUser =
+          await firestore.collection('users').document(currentUser.uid).get();
+      user = User.fromDocument(docUser);
+
+      print(user.name);
+
+      notifyListeners();
     }
-    notifyListeners();
+
   }
-
-
 }
