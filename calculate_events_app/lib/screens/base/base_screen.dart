7319a8cc26005799c8c9c56070b07d3f8bb03391
dandy <danielsbarbosa27@gmail.com>
2021-03-26 01:17:17 -0300

    Exibindo a lista de produtos

--- calculate_events_app/lib/screens/base/base_screen.dart
+++ calculate_events_app/lib/screens/base/base_screen.dart
@@ -1,6 +1,7 @@
 import 'package:calculate_events_app/common/custom_drawer/custom_drawer.dart';
 import 'package:calculate_events_app/models/page_manager.dart';
 import 'package:calculate_events_app/screens/base/login/login_screen.dart';
+import 'package:calculate_events_app/screens/products/products_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -8,7 +9,6 @@ import 'home_screen.dart';
 import 'home_screen.dart';
 
 class BaseScreen extends StatelessWidget {
-
   final PageController _pageController = PageController();
 
   @override
@@ -20,18 +20,7 @@ class BaseScreen extends StatelessWidget {
         controller: _pageController,
         children: [
           Home(),
-          Scaffold(
-            drawer: CustomDrawer(),
-            appBar: AppBar(
-              title: Text('Home0'),
-            ),
-          ),
-          Scaffold(
-            drawer: CustomDrawer(),
-            appBar: AppBar(
-              title: Text('Home1'),
-            ),
-          ),
+          ProductsScreen(),
           Scaffold(
             drawer: CustomDrawer(),
             appBar: AppBar(
